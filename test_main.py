beab8343d3d9023b6afcd3af3cc3849db3da55c9
Sayed-Afnan-Khazi <83779299+Sayed-Afnan-Khazi@users.noreply.github.com>
2023-01-05 20:42:16 +0530

    Updated tests

--- test_main.py
+++ test_main.py
@@ -3,8 +3,8 @@ import main
 
 class TestMain(unittest.TestCase):
     def test_PDA(self):
-        good_samples = ['000111111111111','01111','0'*100+'1'*400,'0'*101+'1'*404,'0'*1000+'1'*4000]
-        bad_samples = ['11','10000','0'*100+'1'*401,'0'*1000+'1'*3999,'0'*10+'1'*46+'0']
+        good_samples = ['0011111111','000111111111111','01111','0'*100+'1'*400,'0'*101+'1'*404,'0'*1000+'1'*4000]
+        bad_samples = ['11','10000','0011001','0'*100+'1'*401,'0'*1000+'1'*3999,'0'*10+'1'*46+'0']
         for sample in good_samples:
             result = main.PDA(sample, print_output = False)
             self.assertEqual(result, 0)
